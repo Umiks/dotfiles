6f372afedaeb1d9a60fcbb686507e91422732235
Евгений Мищенко <evgenjion@yandex-team.ru>
2014-07-10 11:46:44 +0400

    vim configs install

--- launch.sh
+++ launch.sh
@@ -1,9 +1,11 @@
-# TODO: тут будет скрипт для развертывания конфигов
-cd
-mkdir .dotfiles_backup
-cat .vimrc >> .dotfiles_backup/.vimrc
-cat .gitconfig >> .dotfiles_backup/.gitconfig
-cat .bash_profile >> .dotfiles_backup/.bash_profile
-ln -s dotfiles/.vimrc
-ln -s dotfiles/.gitconfig
-ln -s dotfiles/.bash_profile
+# Апаем плагины для вима
+git clone https://github.com/gmarik/Vundle.vim ./.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+#cd
+#mkdir .dotfiles_backup
+#cat .vimrc >> .dotfiles_backup/.vimrc
+#cat .gitconfig >> .dotfiles_backup/.gitconfig
+#cat .bash_profile >> .dotfiles_backup/.bash_profile
+#ln -s dotfiles/.vimrc
+#ln -s dotfiles/.gitconfig
+#ln -s dotfiles/.bash_profile
