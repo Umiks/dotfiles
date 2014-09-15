c82a5c47d06af3798bc55923242197d2a169cb27
Евгений Мищенко <evgenjion@yandex-team.ru>
2014-09-15 21:48:01 +0400

    small fix in launch.bash

--- launch.bash
+++ launch.bash
@@ -16,13 +16,16 @@ fi
 
 # Апаем плагины для вима
 cd "$DOTFILES_DIR"
-git clone https://github.com/gmarik/Vundle.vim "$DOTFILES_DIR/.vim/bundle/Vundle.vim"
-vim +PluginInstall +qall
+if [ -d "$DOTFILES_DIR/.vim/bundle/Vundle.vim" ]; then
+    rm -rf "$DOTFILES_DIR/.vim/bundle/Vundle.vim"
+    git clone https://github.com/gmarik/Vundle.vim "$DOTFILES_DIR/.vim/bundle/Vundle.vim"
+fi
 
 cd ~
 
-mkdir .dotfiles_backup
-[ -e ".vimrc" ] && .vimrc .dotfiles_backup/.vimrc
+[ ! -d ".dotfiles_backup" ] && mkdir .dotfiles_backup
+
+[ -e ".vimrc" ] && mv .vimrc .dotfiles_backup/.vimrc
 [ -e ".gitconfig" ] && mv .gitconfig .dotfiles_backup/.gitconfig
 [ -e ".bash_profile" ] && mv .bash_profile .dotfiles_backup/.bash_profile
 
@@ -30,7 +33,9 @@ ln -s "$DOTFILES_DIR/.vimrc"
 ln -s "$DOTFILES_DIR/.gitconfig"
 ln -s "$DOTFILES_DIR/.bash_profile"
 
+vim +PluginInstall +qall
 
 if [[ $NEED_BASHRC = true ]]; then
+    [ -e ".bashrc" ] && mv .bashrc .dotfiles_backup/.bashrc
     ln -s .bash_profile .bashrc
 fi
